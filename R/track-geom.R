067fe12c787ff84b37c0d8ca83662b3b28984692
Lc0309-coder <3169672818@qq.com>
2025-08-24 20:56:36 +0800

    Constructors
    Constructor of class spPlot、Constructor of class spTrack
    Constructor of spPoints
    Overloading of the show function

--- R/track-geom.R
+++ R/track-geom.R
@@ -10,3 +10,18 @@
 #' @examples
 #' NULL
 setClass("spTrackGeom", slots = c(func = "character", params = "list"))
+
+spPoints <- function(x = NULL,
+                     y = NULL,
+                     pch = 1,
+                     size = unit(0.4,"char"),
+                     gp = gpar()){
+  name_args <-list(
+    x = x,
+    y = y,
+    pch = pch,
+    size = size,
+    gp = gp
+  )
+  new("spTrackGeom", func = "spiral_points", params = c(name_args))
+}
