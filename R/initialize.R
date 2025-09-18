ae4992ca45bb100a835ebc907227cbe9c9a79050
CaoTianze <10096919+CaoTianze@users.noreply.github.com>
2025-09-18 18:14:22 +0800

    document

--- R/initialize.R
+++ R/initialize.R
@@ -2,7 +2,7 @@
 #'
 #'
 #' @slot initMode character. It can only be the following values: "initialize", "initialize_by_gcoor", "initialize_by_time".
-#' @slot initParams list. A **named** list that stores the parameters of the function called by the backend. Based on the value of initMode, the backend function will be one of the following four:[spiralize::spiral_initialize], [spiralize::spiral_initialize_by_gcoor], [spiralize::spiral_initialize_by_time].
+#' @slot initParams list. A **named** list that stores the parameters of the function called by the backend. Based on the value of initMode, the backend function will be one of the following three:[spiralize::spiral_initialize], [spiralize::spiral_initialize_by_gcoor], [spiralize::spiral_initialize_by_time].
 #' @slot tracks list. A list where [spTrack-class] are stored.
 #' @slot clear logical. Whether to call [spiralize::spiral_clear] before drawing.
 #'
